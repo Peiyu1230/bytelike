3608ef3e2e577caf88c486ffcf0fea6723f94627
Peiyu1230 <pyy9512@gmail.com>
2022-05-02 23:13:31 +0800

    update views

--- book/views.py
+++ book/views.py
@@ -5,4 +5,10 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("OK")
+    # return HttpResponse("OK")
+
+    # 模拟数据
+    context ={
+        'name':'有惊喜 快来参加!'
+    }
+    return render(request,'book/index.html',context=context)
