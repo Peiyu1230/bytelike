560194264e94967096814053d61562d147961a13
Peiyu1230 <pyy9512@gmail.com>
2022-05-02 17:37:15 +0800

    book urls update

--- Django_book/urls.py
+++ Django_book/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 from book.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/',index),
+    path('',include('book.urls')),
 ]
