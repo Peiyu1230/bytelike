e068ec31eb3754ea730bddfbc7107702b2d18b97
Peiyu1230 <pyy9512@gmail.com>
2022-05-03 17:01:33 +0800

    添加people表 加入级联操作models.cascade

--- book/models.py
+++ book/models.py
@@ -13,3 +13,19 @@ class BookInfo(models.Model):
     class Meta:
         db_table = "bookinfo"
         verbose_name = "书籍管理"
+
+class PeopleInfo(models.Model):
+    gender_choice = (
+        (1,'male'),
+        (2,'female')
+    )
+
+    name = models.CharField(max_length=10,unique=True)
+    gender = models.SmallIntegerField(choices=gender_choice,default=1)
+    description = models.CharField(max_length=100,null=True)
+    is_delete = models.BooleanField(default=False)
+
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "peopleinfo"
