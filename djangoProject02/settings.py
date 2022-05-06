0ffc6a06861cfeb7099471d9e2fd24dd8f9ee213
Peiyu1230 <pyy9512@gmail.com>
2022-05-06 21:13:57 +0800

    redis setting

--- djangoProject02/settings.py
+++ djangoProject02/settings.py
@@ -129,3 +129,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# redis
+CACHES = {
+    'default':{
+        'BACKEND':'django_redis.cache.RedisCache',
+        'LOCATION':'redis://127.0.0.1:6379/1',
+        'OPTIONS':{
+            'CLIENT_CLASS':'django_redis.client.DefaultClient',
+        }
+    }
+}
+SESSION_ENGINE = 'django.contrib.session.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
